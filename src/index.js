5d4c4dedd443da2dda199128da5178813ad937b9
ali.shabnai <ali.shabani7811@gmail.com>
2023-03-03 14:24:34 +0330

    some change

--- src/index.js
+++ src/index.js
@@ -6,7 +6,9 @@ import reportWebVitals from "./reportWebVitals";
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <BrowserHistory>
+      <App />
+    </BrowserHistory>
   </React.StrictMode>,
   document.getElementById("root")
 );
